872f585b2291462850c897ef16aabdb2963229a5
Alvar Manjon <alvarmanjoni@gmail.com>
2025-04-13 06:55:22 +0200

    rsfsmatrix arreglada y helper genotypematrix()

--- R/rsfsmatrix.R
+++ R/rsfsmatrix.R
@@ -1,13 +1,9 @@
-#' Calculate the rsfs matrix for every sample and every polymorphic site
-#'
-#' @param vcf A vcf file
-#'
-#' @return The rsfs matrix
-#' @export
-rsfsmatrix <- function(vcf) {
+#This is the helper genotypematrix() to build the genotype matrix from the vcf file.
+#It returns the numeric matrix of the amount of alternative alleles per sample.
+genotypematrix <- function(vcf) {
 
-  #beginin of function
-  #Create a matrix with just the columns that contain the sample data
+  #begining of the function
+  #Create a matrix with just the columns that contain the sample genotype data
 
   df <- vcf@gt[,-1]
 
@@ -23,9 +19,40 @@ rsfsmatrix <- function(vcf) {
     }
   }
 
-  #Here we obtain the rsfs as a matrix
+  #Here we obtain the matrix
 
-  rsfs <- matrix(as.integer(df),ncol = ncol(df))     #Convert the strings to integer into a new matrix
-  colnames(rsfs) <- samples                          #Add the samples vector to the column names
-  return(rsfs)                                       #The rsfs
+  genomatrix <- matrix(as.integer(df),ncol = ncol(df))     #Convert the strings to integer into a new matrix
+  colnames(genomatrix) <- samples                          #Add the samples vector to the column names
+
+  return(genomatrix)                                       #The matrix is returned
+
+}
+
+
+#' Calculate the rsfs matrix for every sample and every polymorphic site
+#'
+#' @param vcf A vcf file
+#'
+#' @return The rsfs matrix
+#' @export
+rsfsmatrix <- function(vcf) {
+
+  #First we create the matrix of alternative alleles with the genotypematrix() helper
+
+  rsfs <- genotypematrix(vcf)
+
+  #We substitute every cell which has at least one alternative allele with the sum of the row
+  #For this we iterate the matrix once again
+
+  for(row in 1:nrow(rsfs)) {
+    sumrow <- sum(rsfs[row,], na.rm = TRUE)          #Save the sum of the row before iterating the row
+    for(col in 1:ncol(rsfs)) {
+      if (sum(rsfs[row,col], na.rm = TRUE) > 0) {    #If the cell is not 0 or missing value
+        rsfs[row,col] <- sumrow                      #Transform the cell into the sum of the row
+      }
+    }
+  }
+
+  return(rsfs)                                       #Return the rsfs matrix
+
 }
--- R/sfsmatrix.R
+++ R/sfsmatrix.R
@@ -6,11 +6,12 @@
 #' @export
 sfsmatrix <- function(vcf) {
 
-  #Here we obtain the sfs as a matrix of frequencies from the rsfs matrix calculated from a vcf file
+  #Here we obtain the sfs as a matrix of frequencies from the genotype matrix calculated from a vcf file
+  #We use the helper genotypematrix() over vcf
   #The row values are added together to know the amount of altered polymorphisms. That is converted to a contingency table
   #and then as a matrix
 
-  sfs <- as.matrix(table(rowSums(rsfsmatrix(vcf), na.rm = TRUE), exclude = 0))
+  sfs <- as.matrix(table(rowSums(genotypematrix(vcf), na.rm = TRUE), exclude = 0))
 
   #The column names are changed to add an f so they look like: fi for each frequency
 
